f5cf190b17768cd7be247ac6647210cf8de03d9d
colman423 <colman0423@gmail.com>
2018-06-21 22:00:39 +0800

    parse rank to color

--- pagerank/pagerank.js
+++ pagerank/pagerank.js
@@ -1,31 +1,45 @@
+var map;
+var year = 2012;
 $( () => {
-    var basic_choropleth = new Datamap({
+    map = new Datamap({
         element: $("#pagerank")[0],
         projection: 'mercator',
         fills: {
-            defaultFill: "#ABDDA4",
+            defaultFill: "green"
         },
-        // data: {
-        //     USA: "#456789",
-        //     JPN: { fillKey: "authorHasTraveledTo",
-        //     "electoralVotes": 500 },
-        //     ITA: { fillKey: "authorHasTraveledTo" },
-        //     CRI: { fillKey: "authorHasTraveledTo" },
-        //     KOR: { fillKey: "authorHasTraveledTo" },
-        //     DEU: { fillKey: "authorHasTraveledTo" },
-        // },
+        dataType: 'csv',
+        dataUrl: 'https:///ghost.cs.nccu.edu.tw/~s10329/vis/data.csv',
         geographyConfig: {
-            highlightBorderColor: '#bada55',
+            highlightBorderColor: 'gray',
             popupTemplate: function(geography, data) {
-                return '<div class="hoverinfo">' + geography.properties.name + 'Electoral Votes:' +  data.electoralVotes + ' '
+                return '<div class="hoverinfo">' + geography.properties.name + '\n' +  data[year];
             },
-            highlightBorderWidth: 3
+            highlightBorderWidth: 1
         },
-        done: drawYear
+        done: (map) => {
+            setTimeout( () => {
+                drawYear(map)
+            });
+        }
     });
 
     var colors = d3.scale.category10();
+    map.labels();
 });
-function drawYear() {
-    console.log("done!");
+function drawYear(map) {
+    let data = map.options.data;
+    let color = {};
+    for( country in data ) {
+        let rank = data[country][year];
+        color[country] = parseColor(rank);
+    }
+    console.log(color);
+    map.updateChoropleth(color);
+}
+
+function parseColor(rank) {
+    let total = 222;
+    let percent = rank/total;
+    console.log(percent);
+    return `hsl(0, 100%, ${100-percent*100}%)`;
 }
